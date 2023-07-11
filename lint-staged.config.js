9e89c09ba36e380b4d7a6cbe9b526eb0f193cbc6
Dominik Domagalski <dragenetdev@gmail.com>
2023-07-11 23:42:56 +0200

    #6: Improve lint-staged rules

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,13 +1,20 @@
 module.exports = {
-  // Type check TypeScript files
-  '**/*.(ts|tsx)': () => 'yarn exec "tsc --noEmit"',
+  // Type check and lint then format TypeScript and JavaScript files
 
-  // Lint then format TypeScript and JavaScript files
-  '**/*.(ts|tsx|js)': (filenames) => [
-    `yarn exec "eslint --fix ${filenames.join(' ')}"`,
-    `yarn exec "prettier --write ${filenames.join(' ')}"`,
+  //webapp
+  'packages/webapp/**/*.(ts|tsx|js)': filenames => [
+    `yarn web lint:fix ${filenames.join(' ')}"`,
+    `yarn web prettier ${filenames.join(' ')}"`,
+    `yarn web tscheck`,
+  ],
+
+  'packages/e2e/**/*.(ts|tsx|js)': filenames => [
+    `yarn e2e lint:fix ${filenames.join(' ')}"`,
+    `yarn e2e prettier ${filenames.join(' ')}"`,
+    `yarn e2e tscheck`,
   ],
 
   // Format MarkDown and JSON
-  '**/*.(md|json)': (filenames) => `yarn exec "prettier --write ${filenames.join(' ')}"`,
-};
+  '**/*.(md|json)': filenames =>
+    `yarn exec "prettier --write ${filenames.join(' ')}"`,
+}
