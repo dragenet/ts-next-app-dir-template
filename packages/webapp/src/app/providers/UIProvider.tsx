9e114b251d6bd685940f977f39a8e04ad582010a
Dominik Domagalski <dragenetdev@gmail.com>
2023-07-11 18:11:50 +0200

    #6: Add mantine provider with client components

---
+++ packages/webapp/src/app/providers/UIProvider.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { ReactNode } from 'react';
+import { MantineProvider, useEmotionCache } from '@mantine/core';
+import { useServerInsertedHTML } from 'next/navigation';
+import { CacheProvider } from '@emotion/react';
+
+interface StylesProviderProps {
+  children: ReactNode | string;
+}
+
+export const UIProvider = ({ children }: StylesProviderProps) => {
+  const cache = useEmotionCache();
+  cache.compat = true;
+
+  useServerInsertedHTML(() => (
+    <style
+      data-emotion={`${cache.key} ${Object.keys(cache.inserted).join(' ')}`}
+      dangerouslySetInnerHTML={{
+        __html: Object.values(cache.inserted).join(' '),
+      }}
+    />
+  ));
+
+  return (
+    <CacheProvider value={cache}>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        {children}
+      </MantineProvider>
+    </CacheProvider>
+  );
+};
