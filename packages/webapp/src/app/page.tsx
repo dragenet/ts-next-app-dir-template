57c6a7be7d930fe462374fe230a8553327a275a0
Dominik Domagalski <dragenetdev@gmail.com>
2023-07-17 22:30:09 +0200

    Revert "#6: Add mantine core and hooks"
    This reverts commit 2a4ecc99a8d733ff1de13f20f4642acb5392fb10.

--- packages/webapp/src/app/page.tsx
+++ packages/webapp/src/app/page.tsx
@@ -2,16 +2,15 @@
 
 import { useAppDispatch, useAppSelector } from '@/store'
 import { decrement, increment } from '@/store/slices/testSlice'
-import { Button, Title } from '@mantine/core'
 
 const Page = () => {
   const counterValue = useAppSelector(state => state.counter.value)
   const dispatch = useAppDispatch()
   return (
     <>
-      <Title>{counterValue}</Title>
-      <Button onClick={() => dispatch(increment())}>Increment!!!</Button>
-      <Button onClick={() => dispatch(decrement())}>Decrement!!!</Button>
+      <p>{counterValue}</p>
+      <button onClick={() => dispatch(increment())}>Increment!!!</button>
+      <button onClick={() => dispatch(decrement())}>Decrement!!!</button>
     </>
   )
 }
