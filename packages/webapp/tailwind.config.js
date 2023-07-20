ca1782581d0c830f1fb8ab52aee79b0fb740a68b
Dominik Domagalski <dragenetdev@gmail.com>
2023-07-20 21:17:34 +0200

    #24: Add brand theme

--- packages/webapp/tailwind.config.js
+++ packages/webapp/tailwind.config.js
@@ -4,5 +4,22 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        brand: {
+          primary: '#a1f4b7',
+          secondary: '#098e69',
+          accent: '#b5fcab',
+          neutral: '#1b1a28',
+          'base-100': '#eff1f5',
+          info: '#84a2f5',
+          success: '#2adfa9',
+          warning: '#fbb956',
+          error: '#e5302a',
+        },
+      },
+    ],
+  },
   plugins: [require('daisyui')],
 }
