661af81682dd1a573ff43b05d6c6a00f0d8f3404
Dominik Domagalski <dragenetdev@gmail.com>
2023-07-21 20:43:52 +0200

    #15: Refactor Avatar component

--- packages/webapp/src/components/atoms/Avatar.tsx
+++ packages/webapp/src/components/atoms/Avatar.tsx
@@ -1,13 +1,9 @@
-const Avatar = () => {
-  return (
-    <>
-      <div className="avatar placeholder">
-        <div className="bg-neutral-focus text-neutral-content rounded-full w-10">
-          <span className="text-l">JD</span>
-        </div>
-      </div>
-    </>
-  )
-}
+const Avatar = () => (
+  <div className="avatar placeholder">
+    <div className="bg-neutral-focus text-neutral-content rounded-full w-10">
+      <span className="text-l">JD</span>
+    </div>
+  </div>
+)
 
 export default Avatar
